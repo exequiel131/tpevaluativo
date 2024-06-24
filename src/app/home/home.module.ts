b87cf668c743449a6ae64096b12ef1c16598923e
exequiel131 <exequiellimpayante@gmail.com>
2024-06-24 08:45:13 -0300

    creacion de componentes galeria y home

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,14 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { InicioRoutingModule } from './inicio-routing.module';
 
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule,
-    InicioRoutingModule
+    CommonModule
   ]
 })
-export class InicioModule { }
+export class HomeModule { }
